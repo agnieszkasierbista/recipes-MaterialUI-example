4449ea24a20ddfc31b1b28150172bbc6b760d9bd
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-10-29 13:26:25 +0200

    Add an image gallery to the RecipePage

--- src/components/RecipePage/RecipePage.layout.tsx
+++ src/components/RecipePage/RecipePage.layout.tsx
@@ -1,13 +1,21 @@
 import React, {PropsWithChildren} from 'react';
 import Typography from "@mui/material/Typography";
+import QuiltedImageList from "../ImageList/ImageList.layout";
+import Box from "@mui/material/Box";
 
 
 const RecipePage = (props: any) => {
     return (
-        <Typography variant="h3" component="h1" marginTop={3}>
-dfadfasd
-        </Typography>
+        <>
+            <Typography variant="h3" component="h1" marginTop={3}>
+                Title here
+            </Typography>
+            <Box marginTop={3} sx={{display: "flex"}}>
+                <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="breakfast" height="325"/>
+                <QuiltedImageList/>
+            </Box>
+        </>
     )
-}
+};
 
 export default RecipePage;
